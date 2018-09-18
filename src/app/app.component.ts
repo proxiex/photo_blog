7d56ac6e01f1bc45221cc626317a585b6be73ee4
proxiex <difrentecho@gmail.com>
2018-09-18 13:23:36 +0100

    complete setup
    - modules
    - services
    - forms

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
+})
+export class AppComponent {
+    emoji = ['🎉', '😍', '😜', '👍'];
+    activeEmoji: string;
+    changeEmoji = () => {
+        this.activeEmoji = this.emoji[Math.floor(Math.random() * this.emoji.length)]
+    }
+}
